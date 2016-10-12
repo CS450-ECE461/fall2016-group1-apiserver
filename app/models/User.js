900ac96ff614af55ae0b5aeec343e995b7ed6c49
Brian Foster <fosterbd@iupui.edu>
2016-10-12 12:58:16 -0400

    Add field 'emailAddress'

--- app/models/User.js
+++ app/models/User.js
@@ -1,16 +1,23 @@
 var mongodb = require('@onehilltech/blueprint-mongodb');
+var validator = require('validator');
 
 var schema = new mongodb.Schema({
-  firstName: {
-      type: String,
-      required: true,
-      trim: true
-  },
-  lastName: {
-      type: String,
-      required: true,
-      trim: true
-  }
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    emailAddress: {
+        type: String,
+        required: true,
+        trim: true,
+        validate: validator.isEmail
+    }
 });
 
 module.exports = exports = mongodb.model ('users', schema);
