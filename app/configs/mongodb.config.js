34200cd6d84370552b3141317108505716c3d025
Brian Foster <fosterbd@iupui.edu>
2016-10-12 20:40:56 -0400

    Add MongoDB configuration

---
+++ app/configs/mongodb.config.js
@@ -0,0 +1,24 @@
+var blueprint = require('@onehilltech/blueprint');
+
+module.exports = {
+    connections: {
+        $default: {
+            connstr: 'mongodb://localhost/starter-repo_' + blueprint.env,
+
+            options: {
+                db: {
+                    native_parser: true,
+                    read_preference: "primary",
+                    forceServerObjectId: false,
+                    w: 1
+                },
+                server: {
+                    auto_reconnect: true,
+                    keepAlive: 1,
+                    poolSize: 5,
+                    socketOptions: {}
+                }
+            }
+        }
+    }
+};
