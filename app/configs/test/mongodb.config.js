9e91faf70d9efd289b8ee51df650c44a951713f3
Brian Foster <fosterbd@iupui.edu>
2016-10-24 12:44:20 -0400

    Add testing configuration for MongoDB

---
+++ app/configs/test/mongodb.config.js
@@ -0,0 +1,29 @@
+var blueprint = require('@onehilltech/blueprint');
+
+module.exports = {
+    connections: {
+        $default: {
+            connstr: 'mongodb://localhost/' + blueprint.env,
+            options: {
+                db: {
+                    w: 1,
+                    socketOptions: {
+                        keepAlive: 120
+                    }
+                },
+                server: {
+                    keepAlive: 1,
+                    poolSize: 5,
+                    socketOptions: {
+                        autoReconnect: true,
+                        noDelay: true,
+                        keepAlive: 120,
+                        connectTimeoutMS: 5000,
+                        monitoring: true,
+                        haInterval: 10000
+                    }
+                }
+            }
+        }
+    }
+};
