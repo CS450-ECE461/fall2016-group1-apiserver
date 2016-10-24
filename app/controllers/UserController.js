c8faf09f9a604333ecdc9e47c8283a5a834f7fbe
Brian Foster <fosterbd@iupui.edu>
2016-10-23 21:12:24 -0400

    Add UserController

---
+++ app/controllers/UserController.js
@@ -0,0 +1,13 @@
+var blueprint = require('@onehilltech/blueprint');
+var mongodb = require('@onehilltech/blueprint-mongodb');
+var ResourceController = mongodb.ResourceController;
+var User = require('../models/User');
+
+function UserController() {
+    ResourceController.call(this, {
+        name: "user",
+        model: User
+    });
+}
+
+blueprint.controller(UserController, ResourceController);
