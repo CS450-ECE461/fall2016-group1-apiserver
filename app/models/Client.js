9bd9d6d616b0b12663ea68b760fa9d079f4dcc07
Brian Foster <fosterbd@iupui.edu>
2016-10-25 22:29:59 -0400

    Add preliminary model 'Client'

---
+++ app/models/Client.js
@@ -0,0 +1,38 @@
+var mongodb = require('@onehilltech/blueprint-mongodb');
+var validator = require('validator');
+
+const SECRET_LENGTH = 128;
+
+var schema = new mongodb.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    emailAddress: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        validate: validator.isEmail
+    },
+    roles: {
+        type: [String],
+        required: true,
+        default: []
+    },
+    redirectURL: {
+        type: String,
+        required: true,
+        trim: true,
+        validate: validator.isURL
+    },
+    enabled: {
+        type: Boolean,
+        required: true,
+        default: true
+    }
+});
+
+module.exports = mongodb.model('clients', schema);
