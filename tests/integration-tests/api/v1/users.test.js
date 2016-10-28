2cc312765b4f58e6852c563d786cd71373123b1b
Brian Foster <fosterbd@iupui.edu>
2016-10-27 23:08:51 -0400

    Fix up test suite, include more tests for various methods

--- tests/integration-tests/api/v1/users.test.js
+++ tests/integration-tests/api/v1/users.test.js
@@ -7,9 +7,18 @@ var before = require("mocha").before;
 var describe = require("mocha").describe;
 var users = require('../../../fixtures/users.json');
 
-describe('API v1', function () {
+describe('Route: /api/v1/users', function () {
     var server;
     var request;
+
+    var user = {
+        "handle": "bdfoster",
+        "firstName": "Brian",
+        "lastName": "Foster",
+        "emailAddress": "bdfoster89@gmail.com",
+        "password": "test123"
+    };
+
     before(function (done) {
         async.waterfall([
             function(callback) {
@@ -24,38 +33,71 @@ describe('API v1', function () {
         ], done);
     });
 
-    describe('POST: /api/v1/users', function() {
-        it('should create a user', function(done) {
-            var body = {
-                user: {
-                    _id: "bdfoster",
-                    firstName: "Brian",
-                    lastName: "Foster",
-                    emailAddress: "bdfoster89@gmail.com",
-                    password: "test123!"
+    it('should create a user via POST: /api/v1/users', function(done) {
+        request
+            .post('/api/v1/users')
+            .type('json')
+            .set('Accept', 'application/json')
+            .send({
+                "user": user
+            })
+            .expect(200)
+            .end(function(error, response) {
+                if (error) {
+                    return done(error);
                 }
-            };
 
-            request
-                .post('/api/v1/users')
-                .set('Content-Type', 'application/json')
-                .set('Accept', 'application/json')
-                .send(JSON.stringify(body))
-                .expect(200)
-                .expect('Content-Type', 'application/json')
-                .end(function(error, request) {
-                    if (error) {
-                        return done(error);
-                    }
-                }, done);
-        });
+                var body = response.body;
+                response.status.should.equal(200);
+                user = body.user;
+                done();
+            });
     });
 
-    describe('GET: /api/v1/users', function () {
-        it('should return HTTP status code 404 unconditionally', function (done) {
-            request
-                .get('/api/v1/users')
-                .expect(404, done);
-        });
+    it('should get created user via GET: /api/v1/users/:id', function(done) {
+        request
+            .get('/api/v1/users/' + user._id)
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end(function(error, response) {
+                if (error) {
+                    return done(error);
+                }
+
+                //noinspection JSUnresolvedVariable
+                response.body.user.should.deep.equal(user);
+                done();
+            })
     });
+
+    it('should change \'emailAddress\' field of created user via PUT: /api/v1/users/:id', function(done) {
+        request
+            .put('/api/v1/users/' + user._id)
+            .type('json')
+            .send({
+                "user": {
+                    "emailAddress": "fosterbd@iupui.edu"
+                }
+            })
+            .expect(200)
+            .end(function(error, response) {
+                if (error) {
+                    return done(error);
+                }
+
+                response.body.user.emailAddress.should.equal("fosterbd@iupui.edu")
+                response.body.user.updatedAt.should.not.equal(user.updatedAt);
+                response.body.user.createdAt.should.equal(user.createdAt);
+                user = response.body.user;
+                done();
+            })
+    });
+
+    it('should delete created user via DELETE: /api/v1/users/:id', function(done) {
+        request
+            .delete('/api/v1/users/' + user._id)
+            .expect(200, done);
+    });
+
+
 });
