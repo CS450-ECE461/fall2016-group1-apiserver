f34532efb80af9c35aeecdd3fb8bb939a1162726
Angus Dobson <asdobson@iu.edu>
2016-11-06 18:39:07 -0500

    Updated AuthController to use lib/errors

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -15,7 +15,7 @@ AuthController.prototype.login = function () {
         }, function(err, user, info) {
             if (err) { return next(err); }
             if (!user) {
-                return res.status(422).json({ error: 'Authentication Error.' });
+                return next(new errors.AuthenticationError());
             }
             return res.json({ auth_token: user.createToken() });
         })(req, res, next);
