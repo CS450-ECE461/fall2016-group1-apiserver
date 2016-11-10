b4b1de7195695d9694367e882064915301195203
Brian Foster <fosterbd@iupui.edu>
2016-11-10 18:39:25 -0500

    Remove extra code

--- app/controllers/OrgController.js
+++ app/controllers/OrgController.js
@@ -5,32 +5,7 @@ var _ = require('lodash');
 
 function OrgController() {
     ResourceController.call(this, {
-        model: Org,
-        normalize: {
-            create: [
-                function (request, response, next) {
-                    // Merge query string into Resource document
-                    _.defaultsDeep(request.body, request.query);
-
-                    next();
-                }
-            ],
-            any: [
-                function (request, response, next) {
-                    // Override `_id` with `id` if `_id` does not exist
-                    if (request.body.id && !request.body._id) {
-                        request.body._id = request.body.id;
-                    }
-
-                    // Delete `id`, even if `_id` already existed
-                    if (request.body.id) {
-                        delete request.body.id;
-                    }
-
-                    return next();
-                }
-            ]
-        }
+        model: Org
     });
 }
 
