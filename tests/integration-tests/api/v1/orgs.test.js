75b7cd7410e65be975273415ac2203edcba41eab
Brian Foster <fosterbd@iupui.edu>
2016-11-18 18:04:21 -0500

    Merge branch 'master' of /home/bdfoster/Projects/fall2016-group1-apiserver with conflicts.

--- tests/integration-tests/api/v1/orgs.test.js
+++ tests/integration-tests/api/v1/orgs.test.js
@@ -1,25 +1,30 @@
 var async = require("async");
 var assert = require("chai").assert;
 var blueprint = require("@onehilltech/blueprint");
+var should = require("chai").should();
 var appPath = require("../../../fixtures/appPath");
 var after = require("mocha").after;
 var it = require("mocha").it;
 var before = require("mocha").before;
 var describe = require("mocha").describe;
 var ResourceClient = require("../../../../lib/ResourceClient");
+var _ = require("lodash");
 
 describe("Org API v1", function () {
   var server;
   var agent;
   var userClient;
+  var orgClient;
   var admin = require("../../../fixtures/users")[0];
   var user = require("../../../fixtures/users")[1];
+  var orgs = require("../../../fixtures/orgs");
 
   before(function (done) {
-        // Start server and create clients for User and Org
+    this.timeout(5000);
+    // Start server and create clients for User and Org
     async.waterfall([
       function (callback) {
-                // `app` is returned with callback
+        // `app` is returned with callback
         blueprint.testing.createApplicationAndStart(appPath, callback);
       },
 
@@ -27,13 +32,15 @@ describe("Org API v1", function () {
         server = app.server;
         agent = require("supertest")(server.app);
         userClient = new ResourceClient(agent, "users", 1);
+        orgClient = new ResourceClient(agent, "orgs", 1);
         return callback(null);
       }
     ], done);
   });
 
   before(function (done) {
-        // Create an Admin user and a regular User via API
+    this.timeout(5000);
+    // Create an Admin user and a regular User via API
     async.waterfall([
       function (callback) {
         userClient.create(admin).end(function (error, response) {
@@ -46,11 +53,11 @@ describe("Org API v1", function () {
         });
       },
       function (callback) {
-        userClient.create(user).end(function (error, response) {
+        userClient.create(user).expect(201).end(function (error, response) {
           if (error) {
             return callback(error);
           }
-
+          assert(response.status === 201);
           user["_id"] = response.body.user._id;
           return callback(null);
         });
@@ -59,55 +66,85 @@ describe("Org API v1", function () {
   });
 
   before(function (done) {
-        // Get JWT for admin user
+    this.timeout(5000);
+    // Get JWT for admin user
     agent
-            .post("/api/v1/auth/jwt")
-            .type("json")
-            .send({ username: admin.handle, password: admin.password })
-            .end(function (error, response) {
-              if (error) {
-                return done(error);
-              }
+      .post("/api/v1/auth/jwt")
+      .type("json")
+      .send({username: admin.handle, password: admin.password})
+      .end(function (error, response) {
+        if (error) {
+          return done(error);
+        }
 
-              response.status.should.equal(200);
-              assert(response.body.jwt);
-              admin["jwt"] = response.body.jwt;
-              done();
-            });
+        response.status.should.equal(200);
+        assert(response.body.jwt);
+        admin["jwt"] = response.body.jwt;
+        done();
+      });
   });
 
   before(function (done) {
-        // Get JWT for regular user
+    this.timeout(5000);
+    // Get JWT for regular user
     agent
-            .post("/api/v1/auth/jwt")
-            .type("json")
-            .send({ username: user.handle, password: user.password })
-            .end(function (error, response) {
-              if (error) {
-                return done(error);
-              }
+      .post("/api/v1/auth/jwt")
+      .type("json")
+      .send({username: user.handle, password: user.password})
+      .end(function (error, response) {
+        if (error) {
+          return done(error);
+        }
 
-              response.status.should.equal(200);
-              assert(response.body.jwt);
-              user["jwt"] = response.body.jwt;
-              done();
-            });
+        response.status.should.equal(200);
+        assert(response.body.jwt);
+        user["jwt"] = response.body.jwt;
+        done();
+      });
   });
 
-  it("should not allow creating a org without an authenticated user", function (done) {
-    done();
+  it("should not create an org without an authenticated user", function (done) {
+    orgClient.create({org: orgs[0]}).expect(401, done);
   });
 
-  it("should allow creating an org with an authenticated user", function (done) {
-    done();
+  it("should create an org with an authenticated user", function (done) {
+    orgClient.create({jwt: admin.jwt, org: orgs[0]}).expect(201).end(function (error, response) {
+      if (error) {
+        return done(error);
+      }
+
+      _.each(orgs[0], function (prop) {
+        assert(orgs[0][prop] === response.body.org[prop]);
+      });
+      orgs[0]._id = response.body.org._id;
+      done();
+    });
   });
 
-  it("should allow updating a created org by an org admin", function (done) {
-    done();
+  it("should update a created org by an org admin", function (done) {
+    var emailAddress = "admin@test1.org";
+    orgClient.update(orgs[0]._id, {
+      org: {
+        "emailAddress": emailAddress
+      },
+      jwt: admin.jwt
+    }, function (error, response) {
+      if (error) {
+        return done(error);
+      }
+      assert(response.body.org.emailAddress === emailAddress);
+      orgs[0].emailAddress = emailAddress;
+      done();
+    });
   });
 
-  it("should allow adding users to an org by an org admin", function (done) {
-    done();
+  it("should not update a created org without an authenticated user", function (done) {
+    var emailAddress = "admin2@test1.org";
+    orgClient.update(orgs[0]._id, {
+      org: {
+        "emailAddress": emailAddress
+      }
+    }).expect(401, done);
   });
 
   after(function (done) {
