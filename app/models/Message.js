91ee782bc4897e49ed760226deadf12616e4f445
Angus Dobson <asdobson@iupui.edu>
2016-11-20 23:38:00 -0500

    Added Channel setters

--- app/models/Message.js
+++ app/models/Message.js
@@ -1,5 +1,6 @@
 var mongodb = require("@onehilltech/blueprint-mongodb");
-// var Channel = require("../models/Channel");
+var Channel = require("../models/Channel");
+var User = require("../models/User");
 
 var schema = new mongodb.Schema({
   sender: {
@@ -32,12 +33,52 @@ var schema = new mongodb.Schema({
   timestamps: true
 });
 
-schema.pre("save", function (next) {
-  if (!this.channel) {
+schema.virtual("receivers").set(function (array) {
+  var receivers = [];
+  var invalid = false;
 
+  if (!(array.size > 0)) { return; }
+  if (!(array instanceof Array)) {
+    array = [array];
   }
 
-  return next();
+  // Check each receiver, exit if any are not found
+  var count = 0;
+  for (let id of array) {
+    if (invalid) { return; }
+    User.findByID(id, function (error, result) {
+      if (error) { throw error; }
+      if (!result) {
+        invalid = true;
+        return;
+      }
+      count++;
+      if (!receivers.includes(result._id)) {
+        receivers.push(result._id);
+      }
+      if ((count === array.length) && !invalid) {
+        this.setChannel(receivers);
+      }
+    });
+  };
 });
+
+schema.methods.setChannel = function (receivers) {
+  // See if channel for receivers exists, create one if needed
+  var self = this;
+  Channel.findOne({ members: { $all: receivers } }, function (error, result) {
+    if (error) { throw error; }
+    if (!result) {
+      Channel.create({ members: receivers }, function (error, channel) {
+        if (error) { throw error; }
+        self.channel = channel._id;
+        self.save();
+      });
+    } else {
+      self.channel = result._id;
+      self.save();
+    }
+  });
+};
 
 module.exports = exports = mongodb.model("messages", schema);
