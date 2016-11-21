5feab5075edffbd2c47429ec6162f805bc87bcc6
Angus Dobson <asdobson@iupui.edu>
2016-11-21 01:11:36 -0500

    Fixed post-test cleanup bug

--- tests/integration-tests/api/v1/messages.test.js
+++ tests/integration-tests/api/v1/messages.test.js
@@ -92,7 +92,6 @@ describe("Message API v1", function () {
 
       assert(jwt === messageClient.jwt);
       messages[1].channel = messages[0].channel;
-      messages[1].sender = users[1]._id;
 
       messageClient.create(messages[1]).expect(201).end(function (error, response) {
         if (error) { return done(error); }
@@ -113,7 +112,6 @@ describe("Message API v1", function () {
 
       assert(jwt === messageClient.jwt);
       messages[2].receiver = users[1]._id;
-      messages[2].sender = users[0]._id;
 
       messageClient.create(messages[2]).expect(201).end(function (error, response) {
         if (error) { return done(error); }
@@ -122,6 +120,27 @@ describe("Message API v1", function () {
           assert(messages[2][prop] === response.body.message[prop]);
         });
         messages[2]._id = response.body.message._id;
+        messageClient.deauth();
+        done();
+      });
+    });
+  });
+
+  it("should create another message using receiver", function (done) {
+    messageClient.auth(users[2].emailAddress, users[2].password, function (error, jwt) {
+      if (error) { return done(error); }
+
+      assert(jwt === messageClient.jwt);
+      messages[3].receiver = users[3]._id;
+
+      messageClient.create(messages[3]).expect(201).end(function (error, response) {
+        if (error) { return done(error); }
+
+        _.each(messages[3], function (prop) {
+          assert(messages[3][prop] === response.body.message[prop]);
+        });
+        messages[3]._id = response.body.message._id;
+        messageClient.deauth();
         done();
       });
     });
@@ -129,6 +148,24 @@ describe("Message API v1", function () {
 
   after(function (done) {
     async.waterfall([
+      function (callback) {
+        messageClient.auth(users[0].emailAddress, users[0].password, function (error, jwt) {
+          if (error) {
+            return callback(error);
+          }
+          var count = 0;
+          for (let message of messages) {
+            if (!message._id) { ++count; } else {
+              messageClient.delete(message._id).expect(204).end(function (error) {
+                if (error) {
+                  return callback(error);
+                }
+                if (++count >= messages.length) { return callback(null); }
+              });
+            }
+          }
+        });
+      },
       function (callback) {
         var count = 0;
         for (let user of users) {
@@ -138,19 +175,6 @@ describe("Message API v1", function () {
             }
             if (++count >= users.length) { return callback(null); }
           });
-        }
-      },
-      function (callback) {
-        var count = 0;
-        for (let message of messages) {
-          if (!message._id) { ++count; } else {
-            messageClient.delete(message._id).expect(204).end(function (error) {
-              if (error) {
-                return callback(error);
-              }
-              if (++count >= messages.length) { return callback(null); }
-            });
-          }
         }
       }
     ], done);
