5e0e6e0c04de622a8e145d4b7ba7aef58178af36
Angus Dobson <asdobson@iupui.edu>
2016-11-20 19:38:25 -0500

    Initial channel/message test

---
+++ tests/fixtures/messages.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    "handle": "bdfoster",
+    "firstName": "Brian",
+    "lastName": "Foster",
+    "emailAddress": "fosterbd@iupui.edu",
+    "password": "test1234"
+  },
+
+  {
+    "handle": "asdobson",
+    "firstName": "Angus",
+    "lastName": "Dobson",
+    "emailAddress": "asdobson@iupui.edu",
+    "password": "test1234"
+  },
+
+  {
+    "handle": "mattneal",
+    "firstName": "Matt",
+    "lastName": "Neal",
+    "emailAddress": "mattneal@iupui.edu",
+    "password": "test1234"
+  },
+
+  {
+    "handle": "bdfoster89",
+    "firstName": "Brian",
+    "lastName": "Foster",
+    "emailAddress": "bdfoster89@gmail.com",
+    "password": "test1234"
+  }
+];
---
+++ tests/integration-tests/api/v1/messages.test.js
@@ -0,0 +1,116 @@
+var async = require("async");
+var assert = require("chai").assert;
+var blueprint = require("@onehilltech/blueprint");
+var should = require("chai").should();
+var appPath = require("../../../fixtures/appPath");
+var after = require("mocha").after;
+var it = require("mocha").it;
+var before = require("mocha").before;
+var describe = require("mocha").describe;
+var ResourceClient = require("../../../../lib/ResourceClient");
+var _ = require("lodash");
+
+describe("Message API v1", function () {
+  var server;
+  var agent;
+  var userClient;
+  var messageClient;
+  var user = require("../../../fixtures/users")[1];
+  var messages = require("../../../fixtures/messages");
+
+  before(function (done) {
+    this.timeout(5000);
+    // Start server and create clients for User and Org
+    async.waterfall([
+      function (callback) {
+        // `app` is returned with callback
+        blueprint.testing.createApplicationAndStart(appPath, callback);
+      },
+
+      function (app, callback) {
+        server = app.server;
+        agent = require("supertest")(server.app);
+        userClient = new ResourceClient(agent, "users", 1);
+        messageClient = new ResourceClient(agent, "messages", 1);
+        return callback(null);
+      }
+    ], done);
+  });
+
+  before(function (done) {
+    this.timeout(5000);
+    // Create a User via API
+    async.waterfall([
+      function (callback) {
+        userClient.create(user).expect(201).end(function (error, response) {
+          if (error) {
+            return callback(error);
+          }
+          assert(response.status === 201);
+          user["_id"] = response.body.user._id;
+          return callback(null);
+        });
+      }
+    ], done);
+  });
+
+  // it("should not create an org without an authenticated user", function (done) {
+  //  messageClient.create( {org: orgs[0]} ).expect(401, done);
+  // });
+
+  // it("should create an org with an authenticated user", function (done) {
+  //  orgClient.auth(admin.emailAddress, admin.password, function (error, jwt) {
+  //    if (error) {
+  //      return done(error);
+  //    }
+  //    assert(jwt === orgClient.jwt);
+
+  //    orgClient.create(orgs[0]).expect(201).end(function (error, response) {
+  //      if (error) {
+  //        return done(error);
+  //      }
+
+  //      _.each(orgs[0], function (prop) {
+  //        assert(orgs[0][prop] === response.body.org[prop]);
+  //      });
+  //      orgs[0]._id = response.body.org._id;
+  //      done();
+  //    });
+  //  });
+  // });
+
+  // it("should update a created org by an org admin", function (done) {
+  //  var emailAddress = "admin@test1.org";
+  //  orgClient.update(orgs[0], { "emailAddress": emailAddress }, function (error, response) {
+  //    if (error) {
+  //      return done(error);
+  //    }
+  //    response.body.org.emailAddress.should.equal(emailAddress);
+  //    orgs[0].emailAddress = emailAddress;
+  //    done();
+  //  });
+  // });
+
+  // it("should not update a created org without an authenticated user", function (done) {
+  //  var emailAddress = "admin2@test1.org";
+  //  orgClient.deauth();
+  //  orgClient.update(orgs[0]._id, {
+  //    org: {
+  //      "emailAddress": emailAddress
+  //    }
+  //  }).expect(401, done);
+  // });
+
+  after(function (done) {
+    async.waterfall([
+      function (callback) {
+        userClient.delete(user._id).expect(204).end(function (error) {
+          if (error) {
+            return callback(error);
+          }
+          return callback(null);
+        });
+      }
+    ], done);
+  });
+});
