622f17209d51445279e752c2d078a5168750e5a9
Brian Foster <bdfoster89@gmail.com>
2016-12-05 14:49:26 -0500

    Add method to authorize an authenticated user to modify/delete its own record

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,11 +1,17 @@
 const blueprint = require("@onehilltech/blueprint");
 const ResourceController = require("./../../lib/ResourceController");
 const User = require("../models/User");
+const passport = require("passport");
+const errors = require("../../lib/errors");
 
 function UserController () {
   ResourceController.call(this, {
     model: User,
-    uniques: ["_id", "handle", "emailAddress"]
+    uniques: ["_id", "handle", "emailAddress"],
+    authorize: {
+      update: this.authorizeUserOnModify,
+      delete: this.authorizeUserOnModify
+    }
   });
 }
 
@@ -17,5 +23,21 @@ UserController.prototype.showMe = function () {
     res.json(req.user.toJSON());
   };
 };
+                          
+UserController.prototype.authorizeUserOnModify = function() {
+  return [
+    passport.authenticate("jwt", { session: false }),
+    function(request, response, next) {
+      this.findOneByUnique(request.params.id, function(error, result) {
+        if (error) {
+          return next(errors.normalizeError(error));
+        }
 
+        if (result._id !== request.user._id) {
+          return next(new errors.ForbiddenError());
+        }
+      });
+    }
+  ];
+};
 module.exports = UserController;
